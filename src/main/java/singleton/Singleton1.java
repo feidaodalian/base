b5c379b566316a310b4b7d891ed1417ab267d6e8
feixu <feixu@sohu-inc.com>
2016-02-04 09:47:27 +0800

    bakup

---
+++ src/main/java/singleton/Singleton1.java
@@ -0,0 +1,14 @@
+package singleton;
+
+/**
+ * Created by feixu on 2016/2/1.
+ *
+ */
+public final class Singleton1 {
+    private static Singleton1 singleton1=new Singleton1();
+    private Singleton1(){}
+
+    public static Singleton1 getInstance(){
+        return singleton1;
+    }
+}
